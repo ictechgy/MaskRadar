b74f0df760c0b37b1cc9cbc30953a4bdd437d8a1
ictechgy <ictechgy@gmail.com>
2020-04-23 16:59:18 +0900

    adding licenses

--- app/src/main/java/com/jh/mask_radar/ui/about/LicenseFragment.java
+++ app/src/main/java/com/jh/mask_radar/ui/about/LicenseFragment.java
@@ -10,8 +10,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.jh.mask_radar.R;
+
+import java.util.ArrayList;
 
 public class LicenseFragment extends Fragment {
     RecyclerView recyclerView;
@@ -42,32 +45,50 @@ public class LicenseFragment extends Fragment {
         layoutManager.setOrientation(RecyclerView.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
 
+        ArrayList<LicenseAdapter.License> list = new ArrayList<>();
+
+
+        LicenseAdapter adapter = new LicenseAdapter();
+
+
         return root;
     }
 
-    static class LicenseAdapter extends RecyclerView.Adapter<LicenseAdapter.LicenseHolder>{
+    private static class LicenseAdapter extends RecyclerView.Adapter<LicenseAdapter.LicenseViewHolder>{
+        static class License {
+            String title;
+            String content;
+            License(String title, String content){ this.title = title; this.content = content; }
+        }
+        ArrayList<License> licenses;
 
-        static class LicenseHolder extends RecyclerView.ViewHolder{
-
-            public LicenseHolder(@NonNull View itemView) {
+        static class LicenseViewHolder extends RecyclerView.ViewHolder{
+            TextView licenseTitle;
+            TextView licenseContent;
+            LicenseViewHolder(@NonNull View itemView) {
                 super(itemView);
+                licenseTitle = itemView.findViewById(R.id.license_title);
+                licenseContent = itemView.findViewById(R.id.license_content);
             }
         }
 
         @NonNull
         @Override
-        public LicenseHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null;
+        public LicenseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View cardView = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view_license, parent, false);
+            return new LicenseViewHolder(cardView);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull LicenseHolder holder, int position) {
-
+        public void onBindViewHolder(@NonNull LicenseViewHolder holder, int position) {
+            License license = licenses.get(holder.getAdapterPosition());
+            holder.licenseTitle.setText(license.title);
+            holder.licenseContent.setText(license.content);
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return licenses.size();
         }
     }
 
