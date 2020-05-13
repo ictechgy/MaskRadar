750a17956248a16fa4ae69421dec6b37bb73f4d9
ictechgy <ictechgy@gmail.com>
2020-05-13 23:14:43 +0900

    오류 수정 중

--- app/src/main/java/com/jh/mask_radar/ui/favorite/FavoriteFragment.java
+++ app/src/main/java/com/jh/mask_radar/ui/favorite/FavoriteFragment.java
@@ -1,20 +1,17 @@
 package com.jh.mask_radar.ui.favorite;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -26,6 +23,8 @@ import com.android.volley.toolbox.Volley;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textview.MaterialTextView;
 import com.jh.mask_radar.R;
+import com.jh.mask_radar.databinding.FavoriteInfoViewBinding;
+import com.jh.mask_radar.databinding.FragmentFavoriteBinding;
 import com.jh.mask_radar.db.AppDatabase;
 import com.jh.mask_radar.db.Pharm;
 
@@ -33,12 +32,10 @@ import java.util.List;
 
 public class FavoriteFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener, MaterialButton.OnClickListener{
 
+    private FragmentFavoriteBinding favoriteBinding;
     private FavoriteViewModel favoriteViewModel;
-    private ProgressBar progressBar;
     private AppDatabase db;
-    private RecyclerView recyclerView;
     private RequestQueue requestQueue;
-    private SwipeRefreshLayout refreshLayout;
     private FavoriteAdapter adapter;
     private boolean shoudWait;
     private BlockUpdateTimer timer;
@@ -60,66 +57,61 @@ public class FavoriteFragment extends Fragment implements SwipeRefreshLayout.OnR
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        favoriteBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_favorite, container, false);
         setHasOptionsMenu(true);
         favoriteViewModel =
                 new ViewModelProvider(this).get(FavoriteViewModel.class);
         requestQueue = Volley.newRequestQueue(getContext());
         favoriteViewModel.setQue(requestQueue, getString(R.string.MASK_URL));
         favoriteViewModel.fetchByRoom(db);                               //이렇게 db 객체를 넘겨줘도 괜찮을까? viewModel에서는 지역변수로만 쓰게 하면 괜찮지 않을까?
-
-        View root = inflater.inflate(R.layout.fragment_favorite, container, false);
-        final TextView textView = root.findViewById(R.id.text_no_favorite);
-        recyclerView = root.findViewById(R.id.favorite_recyclerview);
-
-        progressBar = root.findViewById(R.id.favorite_progressBar);
+        favoriteBinding.setFavoriteViewModel(favoriteViewModel);
+        
 
         favoriteViewModel.getPharms().observe(getViewLifecycleOwner(), (pharms -> {
             //store값 변화시 UI 업데이트
             if(pharms == null){      //초기 로딩 시
-                progressBar.setVisibility(View.VISIBLE);
-                refreshLayout.setEnabled(false);
+                favoriteBinding.favoriteProgressBar.setVisibility(View.VISIBLE);
+                favoriteBinding.favoriteSwipeRefreshLayout.setEnabled(false);
             }else if(pharms.size() == 0){                   //즐겨찾기 한 약국이 없을 시
-                textView.setText(getString(R.string.favorite_no_stores));
-                progressBar.setVisibility(View.GONE);
+                favoriteBinding.textNoFavorite.setText(getString(R.string.favorite_no_stores));
+                favoriteBinding.favoriteProgressBar.setVisibility(View.GONE);
                 //즐겨찾기를 모두 삭제시킨 경우 이 부분이 나중에 작동될 수도 있다.
-                refreshLayout.setEnabled(false);
-                recyclerView.setVisibility(View.INVISIBLE);
+                favoriteBinding.favoriteSwipeRefreshLayout.setEnabled(false);
+                favoriteBinding.favoriteRecyclerview.setVisibility(View.INVISIBLE);
             }else {                                      //즐겨찾기 한 약국 표시해주기
                 LinearLayoutManager manager = new LinearLayoutManager(getContext());
                 manager.setOrientation(RecyclerView.VERTICAL);
-                recyclerView.setLayoutManager(manager);
+                favoriteBinding.favoriteRecyclerview.setLayoutManager(manager);
                 adapter = new FavoriteAdapter();
                 adapter.pharms = pharms;
-                recyclerView.setAdapter(adapter);
-                progressBar.setVisibility(View.INVISIBLE);
+                favoriteBinding.favoriteRecyclerview.setAdapter(adapter);
+                favoriteBinding.favoriteProgressBar.setVisibility(View.INVISIBLE);
                 Toast.makeText(getContext(), "업데이트 되었습니다.", Toast.LENGTH_SHORT).show();
-                refreshLayout.setEnabled(true);
-                refreshLayout.setRefreshing(false);
+                favoriteBinding.favoriteSwipeRefreshLayout.setEnabled(true);
+                favoriteBinding.favoriteSwipeRefreshLayout.setRefreshing(false);
                 shoudWait = true;
                 timer.start();
             }
         }));
 
-        refreshLayout = root.findViewById(R.id.favorite_swipe_refresh_layout);
-        refreshLayout.setOnRefreshListener(this);
+        favoriteBinding.favoriteSwipeRefreshLayout.setOnRefreshListener(this);
 
         timer = new BlockUpdateTimer(30000, 30000); //onTick은 쓰지 않을 것이므로 두번째 인자는 첫번째와 동일하게 줌.
 
-        return root;
+        return favoriteBinding.getRoot();
     }
 
 
 
     @Override
     public void onRefresh() {
+        favoriteBinding.favoriteSwipeRefreshLayout.setRefreshing(true);
         if(shoudWait){
             Toast.makeText(getContext(), getString(R.string.block_update), Toast.LENGTH_LONG).show();
-            refreshLayout.setRefreshing(false);
+            favoriteBinding.favoriteSwipeRefreshLayout.setRefreshing(false);
             return;
         }
-        refreshLayout.setRefreshing(true);
         favoriteViewModel.refreshList(db);        //do real update in viewModel
-        refreshLayout.setRefreshing(false);
     }
 
     @Override
@@ -149,34 +141,18 @@ public class FavoriteFragment extends Fragment implements SwipeRefreshLayout.OnR
         private List<Pharm> pharms;
 
         private class FavoriteViewHolder extends RecyclerView.ViewHolder{
-            private MaterialTextView stockStatus;
-            private View statusView;
-            private MaterialTextView updateIcon;
-            private MaterialTextView receiveIcon;
-            private MaterialTextView storeName;
-            private MaterialTextView updateTime;
-            private MaterialTextView receiveTime;
-            private MaterialTextView address;
-            private MaterialButton deleteButton;
-            FavoriteViewHolder(@NonNull View itemView) {
-                super(itemView);
-                stockStatus = itemView.findViewById(R.id.favorite_stock_status);
-                statusView = itemView.findViewById(R.id.favorite_status_view);
-                updateIcon = itemView.findViewById(R.id.favorite_update_icon);
-                receiveIcon = itemView.findViewById(R.id.favorite_receive_icon);
-                storeName = itemView.findViewById(R.id.favorite_store_name);
-                updateTime = itemView.findViewById(R.id.favorite_update_time);
-                receiveTime = itemView.findViewById(R.id.favorite_receive_time);
-                address = itemView.findViewById(R.id.favorite_address);
-                deleteButton = itemView.findViewById(R.id.favorite_delete_button);
+            FavoriteInfoViewBinding infoViewBinding;
+
+            FavoriteViewHolder(@NonNull FavoriteInfoViewBinding infoViewBinding) {
+                super(infoViewBinding.getRoot());
+                this.infoViewBinding = infoViewBinding;
             }
         }
 
         @NonNull
         @Override
         public FavoriteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.favorite_info_view, parent, false);
-            return new FavoriteViewHolder(view);
+            return new FavoriteViewHolder(FavoriteInfoViewBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
         }
 
         @Override
@@ -240,7 +216,7 @@ public class FavoriteFragment extends Fragment implements SwipeRefreshLayout.OnR
 
     @Override
     public void onClick(View v) {
-        RecyclerView.ViewHolder holder = recyclerView.findContainingViewHolder(v);
+        RecyclerView.ViewHolder holder = favoriteBinding.favoriteRecyclerview.findContainingViewHolder(v);
         if(holder==null) {
             Toast.makeText(getContext(), "삭제 처리에 실패하였습니다.", Toast.LENGTH_LONG).show();
             return;
